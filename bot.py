40ed7adbb7afeb2d4596a84d1366c5ad2809e7a7
DerpyChap <holla@derpychap.co.uk>
2019-06-24 20:51:58 +0100

    imgur upload function

--- bot.py
+++ bot.py
@@ -8,6 +8,22 @@ generator = banhammer.Generator()
 with open('config.json') as j:
     config = json.load(j)
 
+async def upload(b):
+    """Upload stuff to Imgur because the GIFs are like 1 MB too large for Discord"""
+    data = aiohttp.FormData(quote_fields=False)
+    data.add_field('image',
+                    b,
+                    filename='banned.gif',
+                    content_type='image/gif')
+    async with aiohttp.ClientSession() as session:
+        async with session.post('https://api.imgur.com/3/image', data=data, headers={'Authorization': config['client_id']}) as resp:
+            if resp.status != 200:
+                return
+            d = await resp.json()
+            if d['success']:
+                return d['data']['link']
+            return
+
 class BotClient(discord.Client):
     async def on_ready(self):
         print('Ready to swing the hammer as', self.user)
